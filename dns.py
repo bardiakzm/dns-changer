68f513d58a1da68ea254b77aac353d3b91d96dfb
bardiakzm <bardiakzm@gmail.com>
2018-12-31 19:34:55 -1200

    Update dns.py

--- dns.py
+++ dns.py
@@ -1,6 +1,19 @@
 import os
-print("keys:\n enter 1 for shekan dns \n enter 2 for 1.1.1.1 and 8.8.8.8 dns \n enter 3 for 8.8.8.8 and 8.8.4.4 dns \n enter 4 for 1.1.1.1 and 1.0.0.1 dns \n")
-a = int(input('which DNS do you want?  :'))
+
+a = int(input("keys:\n"
+          "enter 1 for shekan DNS \n"
+          "enter 2 for 1.1.1.1 and 8.8.8.8 DNS \n"
+          "enter 3 for 8.8.8.8 and 8.8.4.4 DNS \n"
+          "enter 4 for 1.1.1.1 and 1.0.0.1 DNS \n"
+          "enter 5 for OpenDNS  \n"
+          "enter 6 for level3 DNS  \n"
+          "enter 7 for comodo DNS \n"
+          "enter 8 for NortonSafe DNS \n"
+          "enter 9 for yandex DNS \n"
+          ":-->>"))
+
+
+
 if a == 2 :
     os.system('netsh interface ip set dns name="Wi-Fi" source="static" address="1.1.1.1"')
     os.system('netsh interface ip add dns name="Wi-Fi" addr="8.8.8.8" index=2')
@@ -13,6 +26,21 @@ elif a == 3 :
 elif a == 4 :
     os.system('netsh interface ip set dns name="Wi-Fi" source="static" address="1.1.1.1"')
     os.system('netsh interface ip add dns name="Wi-Fi" addr="1.0.0.1" index=2')
+elif a == 5 :
+    os.system('netsh interface ip set dns name="Wi-Fi" source="static" address="208.67.222.222"')
+    os.system('netsh interface ip add dns name="Wi-Fi" addr="208.67.220.220" index=2')
+elif a == 6 :
+    os.system('netsh interface ip set dns name="Wi-Fi" source="static" address="209.244.0.3"')
+    os.system('netsh interface ip add dns name="Wi-Fi" addr="209.244.0.4" index=2')
+elif a == 7 :
+    os.system('netsh interface ip set dns name="Wi-Fi" source="static" address="8.26.56.26"')
+    os.system('netsh interface ip add dns name="Wi-Fi" addr="8.20.247.20" index=2')
+elif a == 8 :
+    os.system('netsh interface ip set dns name="Wi-Fi" source="static" address="199.85.126.10"')
+    os.system('netsh interface ip add dns name="Wi-Fi" addr="199.85.127.10" index=2')
+elif a == 9 :
+    os.system('netsh interface ip set dns name="Wi-Fi" source="static" address="77.88.8.8"')
+    os.system('netsh interface ip add dns name="Wi-Fi" addr="77.88.8.1" index=2')
 elif a == 0 :
     print("this option is not ready yet!")
 
@@ -20,6 +48,18 @@ elif a == 0 :
 
 print("dns changed succfully")    
     
+
+
+        
+       
+
+
+
+
+
+
+
+
 
 
 
